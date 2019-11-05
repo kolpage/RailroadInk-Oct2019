2ceaa24f95627f2818f1ffd8dccd1946e86450d6
Mark Klamik <mark@marks-air-2.lan>
2019-11-04 20:47:39 -0600

    Make canvas kind of draw a road

--- src/client/dice.tsx
+++ src/client/dice.tsx
@@ -25,38 +25,3 @@ export class Dice extends React.Component<IDiceProps> {
     }
 }
 
-const Canvas = () => {
-    return <canvas height='75px' width='75px' />;
-};
-
-interface IDiceProps {}
-
-export class Tile extends React.Component {
-    trainTrackCanvas: any;
-
-    constructor(props: IDiceProps) {
-        super(props);
-        this.trainTrackCanvas = React.createRef();
-    }
-
-    componentDidMount() {
-        if (!this.trainTrackCanvas.current) {
-            return;
-        }
-        const canvas: HTMLCanvasElement = this.trainTrackCanvas.current;
-        const context = canvas.getContext('2d');
-        if (context) {
-            context.fillStyle = 'rgb(200,0,0)';
-            context.fillRect(10, 10, 55, 50);
-        }
-    }
-
-    render() {
-        
-        return (
-            <div className='dice'>
-                <canvas ref={this.trainTrackCanvas}></canvas>
-            </div>
-        )
-    }
-}
--- src/client/tile.tsx
+++ src/client/tile.tsx
@@ -10,10 +10,16 @@ interface IDiceProps {}
 
 export class Tile extends React.Component {
     trainTrackCanvas: any;
+    height: number;
+    width: number;
 
     constructor(props: IDiceProps) {
         super(props);
         this.trainTrackCanvas = React.createRef();
+
+        // TODO: Dont't bastard inject dimensions
+        this.height = 75;
+        this.width = 75;
     }
 
     componentDidMount() {
@@ -23,9 +29,27 @@ export class Tile extends React.Component {
         const canvas: HTMLCanvasElement = this.trainTrackCanvas.current;
         const context = canvas.getContext('2d');
         if (context) {
-            context.fillStyle = 'rgb(200,0,0)';
-            context.fillRect(10, 10, 55, 50);
+           this.drawRoad(context);
         }
+    }
+
+    drawRoad(context: CanvasRenderingContext2D) {
+        this.drawLine(this.width/3, this.width/3, 0, this.height, context);
+        this.drawDottedLine(10, this.width/2, this.width/2, 0, this.height, context);
+        this.drawLine(this.width*(2/3), this.width*(2/3), 0, this.height, context);
+    }
+
+    drawDottedLine(segLength: number, xStart: number, xEnd: number, yStart: number, yEnd: number, context: CanvasRenderingContext2D) {
+        context.setLineDash([10]);
+        this.drawLine(xStart, xEnd, yStart, yEnd, context);
+        context.setLineDash([]);
+    }
+
+    drawLine(xStart: number, xEnd: number, yStart: number, yEnd: number, context: CanvasRenderingContext2D) {
+        context.beginPath();
+        context.moveTo(xStart,yStart);
+        context.lineTo(xEnd, yEnd);
+        context.stroke();
     }
 
     render() {
