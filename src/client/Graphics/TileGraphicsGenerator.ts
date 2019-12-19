9210de0c3795398155c65cc0aebf4584e56f73a2
Mark Klamik <mklams@gmail.com>
2019-12-18 21:14:41 -0600

    Clean up tile graphics. Start breaking up god class

---
+++ src/client/Graphics/TileGraphicsGenerator.ts
@@ -0,0 +1,12 @@
+import { Orientation, TileType } from "../../common/Enums";
+
+export class TileGraphicsFactory {
+    constructor(context: CanvasRenderingContext2D, length: number) {
+        
+    }
+
+    public GetTileGraphic(tileType: TileType) {
+        
+
+    }
+}
