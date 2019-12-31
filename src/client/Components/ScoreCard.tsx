dc34565255fb0dc024b92b4976d2948b09cbd850
Mark Klamik <mklams@gmail.com>
2019-12-31 16:42:38 -0600

    Add score card style

--- src/client/Components/ScoreCard.tsx
+++ src/client/Components/ScoreCard.tsx
@@ -1,11 +1,39 @@
 import * as React from 'react';
+import { ScoreDTO } from '../../common/DTO/ScoreDTO';
+import '../styles/scoreCard.scss';
 
-interface IScoreCardProps{
-    score: number;
-}
-
-export default function ScoreCard(props: IScoreCardProps){
+// Future: Create a sepereate model from ScoreDTO 
+export default function ScoreCard(props: ScoreDTO){
     return(
-        <div>{props.score}</div>
+        <table>
+            <tr>
+                <td>{"Exit"}</td>
+                <td>{props.ExitScore}</td>
+            </tr>
+            <tr>
+                <td>{"Longest Road"}</td>
+                <td>{props.LongestRoadScore}</td>
+            </tr>
+            <tr>
+                <td>{"Longest Rail"}</td>
+                <td>{props.LongestRailScore}</td>
+            </tr>
+            <tr>
+                <td>{"Center Squares"}</td>
+                <td>{props.CenterSquareScore}</td>
+            </tr>
+            <tr>
+                <td>{"Errors"}</td>
+                <td>{props.ErrorScore}</td>
+            </tr>
+            <tr>
+                <td>{"Expansion"}</td>
+                <td>{props.ExpansionScore}</td>
+            </tr>
+            <tr>
+                <td>{"Total"}</td>
+                <td>{props.TotalScore}</td>
+            </tr>
+        </table>
     );
 }
