406e147a1937d30e380795f62a13ac74d415978d
Mark Klamik <mklams@gmail.com>
2020-01-08 08:34:47 -0600

    Add train animation

--- src/client/Components/StartMenu.tsx
+++ src/client/Components/StartMenu.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react';
 import '../styles/startMenu.scss';
 
+const TrainImg = require("../Assests/Train.png")
+const CarImg = require("../Assests/Car.png")
+
 interface IStartMenuPros{
     startGame: (seed: string) => void;
 }
@@ -19,6 +22,10 @@ export default function StartMenu(props: IStartMenuPros){
     return(
         <div className="centerBox">
             <div className="welcomeMessage">Welcome to RailRoad Ink!</div>
+            <div>
+                <figure><img src={TrainImg} className='trainImg' /></figure>
+                {/*<figure><img src={CarImg} className='carImg' /></figure>*/}
+            </div>
             <button className="startButton" onClick={startGameClicked}>Start Game</button>
             <div className="seedOption">
                 <span title="Leave empty to get a random seed">Seed: </span>
